6a01ead54a68a122cf1edda0eaf97a66bd2e1abb
Lochan <98641736+Lochan005@users.noreply.github.com>
2026-04-07 22:23:29 +0530

    favicon update

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,13 +12,13 @@ export default function manifest(): MetadataRoute.Manifest {
     orientation: "portrait-primary",
     icons: [
       {
-        src: "/icon",
+        src: "/icon.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "/icon",
+        src: "/icon.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "any",
